098cc48380eab4ae49c64a4a63a234aca1df6e81
Wbwren <wren233@gmail.com>
2021-04-26 11:17:25 +0100

    Feat: add login functionality.

--- app.py
+++ app.py
@@ -35,34 +35,59 @@ print(get_price('BTC', 'ETH', 'XMR'))
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('base.html')
+  return render_template('base.html')
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
-        # check if username already exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+  if request.method == "POST":
+    # check if username already exists in db
+    existing_user = mongo.db.users.find_one(
+      {"username": request.form.get("username").lower()})
 
-        if existing_user:
-            flash("Username already exists")
-            return redirect(url_for("register"))
+    if existing_user:
+      flash("Username already exists")
+      return redirect(url_for("register"))
 
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
+    register = {
+      "username": request.form.get("username").lower(),
+      "password": generate_password_hash(request.form.get("password"))
+    }
+    mongo.db.users.insert_one(register)
 
-        # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
-        return render_template('dashboard.html')
+    # put the new user into 'session' cookie
+    session["user"] = request.form.get("username").lower()
+    flash("Registration Successful!")
+    return render_template('dashboard.html')
 
-    return render_template("register.html")
+  return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if request.method == "POST":
+    # check if username exists in db
+    existing_user = mongo.db.users.find_one(
+      {"username": request.form.get("username").lower()})
+
+    if existing_user:
+      # ensure hashed password matches user input
+      if check_password_hash(
+        existing_user["password"], request.form.get("password")):
+          session["user"] = request.form.get("username").lower()
+          flash("Welcome, {}".format(request.form.get("username")))
+          return render_template('dashboard.html')
+      else:
+        # invalid password match
+        flash("Incorrect Username and/or Password")
+        return redirect(url_for("login"))
+
+    else:
+      # username doesn't exist
+      flash("Incorrect Username and/or Password")
+      return redirect(url_for("login"))
+
+  return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
